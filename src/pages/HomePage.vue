c67f2c5354ac0a20d216ce743e0a2302ca3ec60e
KrowFeather <2625056626@qq.com>
2024-12-30 18:23:08 +0800

    accept withdraw

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -21,10 +21,32 @@
             </template>
           </el-table-column>
         </el-table>
-
       </el-tab-pane>
-      <el-tab-pane label="Discarded" name="third">Role</el-tab-pane>
-      <el-tab-pane label="Accepted" name="fourth">Task</el-tab-pane>
+      <el-tab-pane label="Discarded" name="third">
+        <el-table :data="withdrawProposals" style="width: 90%">
+          <el-table-column prop="id" label="ID" width="50"></el-table-column>
+          <el-table-column prop="depart" label="始发"></el-table-column>
+          <el-table-column prop="destination" label="目的地"></el-table-column>
+          <el-table-column prop="totalPrice" label="总价"></el-table-column>
+          <el-table-column prop="ticketPrice" label="票价"></el-table-column>
+          <el-table-column prop="days" label="天数"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="Accepted" name="fourth">
+        <el-table :data="acceptProposals" style="width: 90%">
+          <el-table-column prop="id" label="ID" width="50"></el-table-column>
+          <el-table-column prop="depart" label="始发"></el-table-column>
+          <el-table-column prop="destination" label="目的地"></el-table-column>
+          <el-table-column prop="totalPrice" label="总价"></el-table-column>
+          <el-table-column prop="ticketPrice" label="票价"></el-table-column>
+          <el-table-column prop="days" label="天数"></el-table-column>
+          <el-table-column label="operation">
+            <template #default="scope">
+              <el-button type="success" size="small" @click="acceptProposal(scope.row)">付款</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -36,9 +58,13 @@ import { onMounted, ref } from 'vue';
 const activeName = ref('first')
 const depart = ref('')
 const destination = ref('')
-const proposals = ref()
+const proposals = ref([])
+const withdrawProposals =ref([])
+const acceptProposals = ref([])
 onMounted(async () => {
   await getAllWaitingProposals()
+  await getAllAcceptProposals()
+  await getAllWithdrawProposals()
 })
 const generate_proposal = async () => {
   await axios.get(customer_url + '/api/customers/get_proposal', {
@@ -63,8 +89,44 @@ const getAllWaitingProposals = async () => {
   })
 }
 
+const getAllAcceptProposals = async () => {
+  const cid = sessionStorage.getItem('cid')
+  await axios.get(customer_url + '/api/customers/all_accepted_proposal', {
+    params: {
+      id: cid
+    }
+  }).then((resp) => {
+    console.log(resp.data)
+  })
+}
+
+const getAllWithdrawProposals = async () => {
+  const cid = sessionStorage.getItem('cid')
+  await axios.get(customer_url + '/api/customers/all_withdrawed_proposal', {
+    params: {
+      id: cid
+    }
+  }).then((resp) => {
+    console.log(resp.data)
+  })
+}
+const acceptProposal = (row)=>{
+  axios.post(customer_url+'/api/customers/accept/'+row.id).then((resp)=>{
+    console.log(resp.data)
+  })
+}
+
+const rejectProposal = (row)=>{
+  axios.post(customer_url+'/api/customers/withdraw/'+row.id).then((resp)=>{
+    console.log(resp.data)
+  })
+}
+
 const eventSource = new EventSource(customer_url + '/api/customers/subscribe/' + sessionStorage.getItem('cid'));
 const eventSource1 = new EventSource(customer_url + '/api/customers/waiting_proposal_subscribe/' + sessionStorage.getItem('cid'));
+const eventSource2 = new EventSource(customer_url + '/api/customers/refresh_proposal_subscribe/' + sessionStorage.getItem('cid'));
+const eventSource_accept = new EventSource(customer_url + '/api/customers/accept_proposal_subscribe/' + sessionStorage.getItem('cid'));
+const eventSource_withdraw = new EventSource(customer_url + '/api/customers/withdraw_proposal_subscribe/' + sessionStorage.getItem('cid'));
 
 eventSource.onmessage = function (event) {
   console.log("Received SSE message:", event.data);
@@ -83,6 +145,24 @@ eventSource1.onmessage = function (event) {
     message: 'all waiting Proposal refreshed',
     type: 'success'
   })
+};
+
+eventSource2.onmessage =async function (event) {
+  console.log("ok")
+  console.log("Received SSE message:", event.data);
+  await getAllWaitingProposals()
+  await getAllAcceptProposals()
+  await getAllWithdrawProposals()
+};
+eventSource_accept.onmessage =async function (event) {
+  acceptProposals.value = JSON.parse(event.data)
+  console.log("ok")
+  console.log("Received SSE message:", event.data);
+};
+eventSource_withdraw.onmessage =async function (event) {
+  withdrawProposals.value = JSON.parse(event.data)
+  console.log("ok")
+  console.log("Received SSE message:", event.data);
 };
 </script>
 <style scoped></style>
